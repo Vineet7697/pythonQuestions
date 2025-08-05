42558fadbf2a6bda434a9addfbb8d798d0e948ae
vineet kushwaha <vineetkushwaha2010@gmail.com>
2025-08-05 11:09:17 +0530

    few changes

--- Anagram.py
+++ Anagram.py
@@ -31,3 +31,15 @@ else:
        print("not anagram")
 
 
+
+#using function
+def anagram(r,t):
+    if len(r)!=len(t):
+        return False
+    if sorted(s1)==sorted(s2):
+        return True
+    return False
+
+s1="listen"
+s2="silent"
+print(anagram(s1,s2))
